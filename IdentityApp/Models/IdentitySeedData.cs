0808adf3fa69039fbf6b2a128b6dfb7cac31ffa0
Mehmet Nail <iskecem@hotmail.com>
2025-02-27 14:16:52 +0200

    Add project files.

---
+++ IdentityApp/Models/IdentitySeedData.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace IdentityApp.Models
+{
+    public static class IdentitySeedData
+    {
+        private const string adminUser = "Admin";
+        private const string adminPassword = "Admin_123";
+
+        public static async void IdentityTestuser(IApplicationBuilder app)
+        {
+            var context = app.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<IdentityContext>();
+
+            if (context.Database.GetAppliedMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+        }
+    }
+}
